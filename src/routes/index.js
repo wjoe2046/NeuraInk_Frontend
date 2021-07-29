5040cf2d49469a40787eabec5c362c1cfee6b8db
Wenfei Zhou <wzhou2046@gmail.com>
2021-07-29 14:36:39 -0400

    minor changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,16 @@
-import React, { lazy, Suspense } from "react";
-import { Switch, Route } from "react-router-dom";
-import FallbackSpinner from "components/fallbackSpinner";
+import React, { lazy, Suspense } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import FallbackSpinner from 'components/fallbackSpinner';
 
-const Home = lazy(() => import("pages/home"));
-const Landing = lazy(() => import("pages/landing"));
+const Home = lazy(() => import('pages/Home'));
+const Landing = lazy(() => import('pages/Landing'));
 
 const Routes = () => {
   return (
     <Suspense fallback={<FallbackSpinner />}>
       <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/dashboard" component={Home} />
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/dashboard' component={Home} />
       </Switch>
     </Suspense>
   );
